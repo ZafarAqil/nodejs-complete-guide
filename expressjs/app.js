d0431e0542d666dd0875ac14d3910c8ebf3b9a2e
Zafar Aqil <zafaraqil14@gmail.com>
2019-06-11 18:35:51 +0530

    added routing and static files

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,15 +1,25 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const rootDir = require("./util/path");
+
+const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
 
 app.listen(3000);
